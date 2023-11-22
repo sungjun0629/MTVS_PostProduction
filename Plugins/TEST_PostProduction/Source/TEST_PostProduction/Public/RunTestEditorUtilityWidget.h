6f881f1209207a73a6c5aff11f5f1f52322ab07b
obdo <81908835+fortress43-dev@users.noreply.github.com>
2023-11-22 18:46:43 +0900

    Run Editor BP Test

---
+++ Plugins/TEST_PostProduction/Source/TEST_PostProduction/Public/RunTestEditorUtilityWidget.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "RunTestEditorUtilityWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_POSTPRODUCTION_API URunTestEditorUtilityWidget : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+	
+	
+};
